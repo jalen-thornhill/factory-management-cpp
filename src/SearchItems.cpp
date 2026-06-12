cc9116e1a9dba58cf8250cf40b2f8530178149f2
Jalen Thornhill <by.jalen.by@outlook.com>
2026-06-12 15:20:46 -0400

    feat: add search items class shell

---
+++ src/SearchItems.cpp
@@ -0,0 +1,11 @@
+/**
+ * File: SearchItems.cpp
+ * Author: Jalen Thornhill
+ * Created: 2026-06-12
+ * Last Modified: 2026-06-12
+ */
+
+#include "SearchItems.hpp"
+
+SearchItems::SearchItems() {
+}
---
+++ src/SearchItems.hpp
@@ -0,0 +1,15 @@
+/**
+ * File: SearchItems.hpp
+ * Author: Jalen Thornhill
+ * Created: 2026-06-12
+ * Last Modified: 2026-06-12
+ */
+
+#pragma once
+
+#include "Item.hpp"
+
+class SearchItems {
+public:
+    SearchItems();
+};
